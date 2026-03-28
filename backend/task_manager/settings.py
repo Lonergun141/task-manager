f494a6d9c0999e563dfd20bfb752b92cd1c9b391
Clyde Gevero <gevero530@gmail.com>
2026-03-28 21:02:00 +0800

    feat: initialize tasks app with Task model, serializer, and Django configuration

--- backend/task_manager/settings.py
+++ backend/task_manager/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-qw7ofi#ah*i)r5os0=)*)w944mpom6g3$wn48o=3stmkt(t)!_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 
 # Application definition
@@ -37,9 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
+    'tasks',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware', 
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
